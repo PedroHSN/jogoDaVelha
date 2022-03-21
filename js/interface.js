70fbc318a311c31c8e30d582d694b64f82718f68
pedrohsn <pedrodti2017@gmail.com>
2022-03-21 01:21:27 -0300

    removi alguns comentarios

--- js/interface.js
+++ js/interface.js
@@ -16,31 +16,13 @@ function handleClick(event){
         setTimeout(()=>{
           alert("O jogo acabou - O Vencedor foi " + playerTime);
         },10);
-        
       }
-      updateSquare(position);
+    updateSquare(position);
 }
 
-function updateSquare(position){ // essa função atualiza apeans um quadrado, ao contrario
-                                // da função de baixos que atualiza todos os quadrados.
+function updateSquare(position){ 
   let square = document.getElementById(position.toString());
   let symbol = board[position];
   square.innerHTML = `<div class='${symbol}'></div>`
 
 }
-
-// function updateSquares(){
-
-//   let squares = document.querySelectorAll(".square");
-
-//     squares.forEach((square) => {
-
-//         let position = square.id
-//         let symbol = board[position];
-
-//         if(symbol != ''){
-            
-//         }
-//     })
-
-// }
