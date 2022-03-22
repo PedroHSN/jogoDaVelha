1e21ca6e3d91ebc60dfbe033b6e541948d4819ee
pedrohsn <pedrodti2017@gmail.com>
2022-03-22 01:03:51 -0300

    comentando todas as funcionalidades no codigo

--- js/game.js
+++ js/game.js
@@ -1,10 +1,10 @@
 //iniciar variáveis
-let board = ['', '', '', '', '', '', '', '', ''];
-let playerTime = 0;
-let symbols = ['o', 'x'];
-let gameOver = false;
+let board = ['', '', '', '', '', '', '', '', '']; // espaços em branco do tabuleiro.
+let playerTime = 0; // jogador da vez (0 ou 1).
+let symbols = ['o', 'x']; // simbolos de cada jogador.
+let gameOver = false; // gameOver vai ser false primeiro.
 
-let winStates = [
+let winStates = [ //todas as possibiliades de vitoria (em relação as posições)
   [0,1,2],
   [3,4,5],
   [6,7,8],
@@ -15,7 +15,7 @@ let winStates = [
   [2,4,6],
 ]
 
-function handleMove(position){
+function handleMove(position){ //a função handlemove quer dizer o ato de colocar um simbolo numa posição do board
 
     if(gameOver){
       return;
@@ -23,35 +23,36 @@ function handleMove(position){
 
     if(board[position] == ''){
       board[position] = symbols[playerTime];
-      gameOver = isWin();
+      // LEIA-SE: se a posição x for vazia, a posição x vai receber um simbolo do playertime da vez.
+
+      gameOver = isWin(); //verificação se teve uma sequencia vitoriosa.
+
       if(!gameOver){
 
-        playerTime = (playerTime == 0) ? 1 : 0 // isso substitui o comentario abaixo.
+        playerTime = (playerTime == 0) ? 1 : 0 
+        // LEIA-se: se o game over for falso, o playertime vai trocar de 0 pra 1 sucessivamente
 
-        // if(playerTime == 0){
-        //     playerTime = 1;
-        // }else {
-        //     playerTime = 0;
-        // }
       }
   }
   return gameOver;
 }
 
-function isWin(){
+function isWin(){ //Função verificar vencedor
 
   for(let i=0; i<winStates.length; i++){
-    let seq = winStates[i];
+    let seq = winStates[i];//em cada rodad o winStates vai ler se tem alguma sequencia vitoriosa.
 
-    let pos1 = seq[0];
-    let pos2 = seq[1];
-    let pos3 = seq[2];
+    let pos1 = seq[0];//aqui le se a posição [0] corresponde a sequencia [0] vitoriosa.
+    let pos2 = seq[1];//aqui le se a posição [1] corresponde a sequencia [1] vitoriosa.
+    let pos3 = seq[2];//aqui le se a posição [2] corresponde a sequencia [2] vitoriosa.
 
     if(board[pos1] == board[pos2] && board[pos1] == board[pos3] && board[pos1] != ''){
 
         return true;
     }
   }
-
       return false;
 }
+
+
+// ESSE JOGO FOI CRIADO COM MVC (MODEL VIEW CONTROLER)
--- js/interface.js
+++ js/interface.js
@@ -1,18 +1,23 @@
 document.addEventListener('DOMContentLoaded', () => {
+  // ação sera executada quando o documento for carregado
 
     let squares = document.querySelectorAll(".square");
+    // metodo que retorna uma node list (está selecionando todas as classes .square)
 
     squares.forEach((square) => {
+      // O método forEach() pegará cada square do QuerySelectorAll
+
         square.addEventListener('click', handleClick);
+        // em cada square do .forEach, será adicionado o evento click e a função handleClick
     })
 })
 
 function handleClick(event){
 
-    let square = event.target;
-    let position = square.id;
+    let square = event.target; // event.target = elemento que sofreu a ação/evento (no caso o .square/quadrado)
+    let position = square.id; //position no arquivo game será igual ao quadrado do board + id do html
 
-    if(handleMove(position)){
+    if(handleMove(position)){ //a função handleMove quer dizer o ato de colocar um simbolo numa posição do board
         setTimeout(()=>{
           alert("O jogo acabou - O Vencedor foi " + playerTime);
         },10);
